02e5369d80779d388909aaf3212f15a1ea25a09d
bearman <campoltro@gmail.com>
2026-05-09 19:13:25 -0600

    v735: Notificación solsticios personales (A) + manifest orientation any (auto-rotate)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'astro-atlas-v732';
+const CACHE_NAME = 'astro-atlas-v735';
 const ASSETS = [
   '/astro-atlas/',
   '/astro-atlas/index.html',
--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'astro-atlas-v732';
+const CACHE_NAME = 'astro-atlas-v735';
 const ASSETS = [
   '/',
   '/index.html',
